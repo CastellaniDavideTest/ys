dcadb3bc2a2127bffd8a3292e3abca9466775236
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-14 14:38:21 +0100

    Created bin/ys.py

---
+++ bin/ys.py
@@ -0,0 +1,19 @@
+"""ys
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-14"
+
+class ys:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(ys.ys())
+	
+	def ys():
+		"""ys first funtion
+		"""
+		return "ys" #for the test (see test_ys.py)
+		
+if __name__ == "__main__":
+	ys()
